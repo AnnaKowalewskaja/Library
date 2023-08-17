4dddb5c0ebb96d1b0a5ac3bd1911f89a54bc260b
Anna <kowalewskajaanna@gmail.com>
2023-08-17 13:30:36 +0200

    fix:slider

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+
+/* burger menu */
+
 let burger = document.querySelector('.burger');
 let menu = document.querySelector('.nav');
 let menuLinks = document.querySelectorAll('.nav__list-link');
@@ -32,3 +35,41 @@ body.addEventListener('click', (e) => {
         openMenu();
     }
 });
+
+/*slider */
+
+const slider = document.querySelector('.slider');
+
+const prevButton = document.querySelector('.prev-button');
+const nextButton = document.querySelector('.next-button');
+const slides = Array.from(slider.querySelectorAll('img'));
+const slideCount = slides.length;
+let slideIndex = 0;
+
+prevButton.addEventListener('click', showPreviousSlide);
+nextButton.addEventListener('click', showNextSlide);
+
+function showPreviousSlide() {
+    slideIndex = (slideIndex - 1 + slideCount) % slideCount;
+    updateSlider();
+}
+
+function showNextSlide() {
+    slideIndex = (slideIndex + 1) % slideCount;
+    updateSlider();
+}
+
+function updateSlider() {
+    slides.forEach((slide, index) => {
+        if (index === slideIndex) {
+            slide.style.display = 'block';
+
+
+        } else {
+             slide.style.display = 'none';
+            
+        }
+    });
+}
+
+updateSlider();
